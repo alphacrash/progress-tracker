d7dc5a3601aa3da4b570dfb1edb56c85fe99d782
alphacrash <sudhansu.ace@gmail.com>
2025-04-10 15:28:50 +0530

    Removed Dark Theme

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -4,7 +4,7 @@ const Progress = ({ current, total, hide = false, overall }) => {
   return (
     <div className="flex items-center gap-x-3 whitespace-nowrap">
       <div
-        className="flex w-full h-2 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700"
+        className="flex w-full h-2 bg-gray-200 rounded-full overflow-hidden "
         role="progressbar"
         aria-valuenow={percentage}
         aria-valuemin="0"
@@ -13,17 +13,17 @@ const Progress = ({ current, total, hide = false, overall }) => {
         <div
           className={`flex flex-col justify-center rounded-full overflow-hidden text-xs text-white text-center whitespace-nowrap transition duration-500 ${
             overall
-              ? "bg-emerald-300 dark:bg-emerald-500"
+              ? "bg-emerald-300 "
               : percentage === 100
-              ? "bg-green-600 dark:bg-green-500"
-              : "bg-blue-600 dark:bg-blue-500"
+              ? "bg-green-600 "
+              : "bg-blue-600 "
           }`}
           style={{ width: `${percentage}%` }}
         ></div>
       </div>
       {!hide && (
         <div className="w-24 text-end">
-          <span className="text-sm text-gray-800 dark:text-white">
+          <span className="text-sm text-gray-800">
             {current}/{total}
           </span>
         </div>
